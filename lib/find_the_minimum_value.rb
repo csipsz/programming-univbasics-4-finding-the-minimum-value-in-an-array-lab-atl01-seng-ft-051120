e025e7408ac27f4a1eeac3cd54765831991e3f2e
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-31 15:38:47 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,9 +1,11 @@
 def find_min_value(array)
   count = 0 
-  
+  min = array[0] 
   while count < array.length do 
-    
-    if 
-    
+    if min > array[count]
+      min = array[count]
+    end
+    count += 1
   end
+  return min 
 end
